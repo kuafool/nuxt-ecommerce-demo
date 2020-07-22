3ec9b9a9abde6d1be37f77a3b67f1bdb9e138c0c
sunchaser <sunchaserkuafu@outlook.com>
2020-07-22 13:24:38 +0800

    update title

--- config/nuxt.config.head.js
+++ config/nuxt.config.head.js
@@ -3,7 +3,7 @@ export default {
    ** Headers of the page
    */
   head: {
-    title: process.env.npm_package_name || '',
+    title: 'E-Commerce demo site built with Nuxt.js.',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
